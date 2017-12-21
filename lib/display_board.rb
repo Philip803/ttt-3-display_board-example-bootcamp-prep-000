cd392dd964f67c2521d3bdc61d691ad26ae8a482
philip ka sing leung <philip.leung90@gmail.com>
2017-12-21 19:02:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@ welcome = "Welcome To Tic Tac Toe"
 
 def display_board
   puts "   |   |   "
-  puts "---------"
+  puts "-----------"
   puts "   |   |   "
-  puts "---------"
+  puts "-----------"
   puts "   |   |   "
   puts "What move would X like to make (1-9)?"
 end
